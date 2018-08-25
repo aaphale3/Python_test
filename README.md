a8fbe45f38879d1290e02f573f0c5cb715427a34
aditya aphale <aaphale3@gmail.com>
2018-08-25 14:20:32 -0400

    prime.py gives you a count of prime numbers between 0 and 100 - initial version

---
+++ README.md
@@ -0,0 +1,7 @@
+#This is my first git project.
+
+## Running
+
+This project will tell you the number of prime numbers between 1 and 100.
+
+'python.prime.py'
---
+++ prime.py
@@ -0,0 +1,9 @@
+all_nums = list(range(2, 101)) # create
+prime_list = list(range(2, 101))
+for num in all_nums:
+    if num in (2, 3, 5, 7):
+        continue
+    elif (num % 5 == 0) or (num % 3 == 0) or (num % 2 == 0) or (num % 7 == 0):
+        prime_list.remove(num)
+
+print(len(prime_list))
